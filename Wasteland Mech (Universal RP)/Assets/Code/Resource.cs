9ab236b9b69653dd519712d3e0f19c41f824d109
Bryan <45637546+Dragola@users.noreply.github.com>
2020-08-22 19:17:15 -0400

    Swapped to Universal Render Pipeline + fixed issue with swapping buildings
    Changes:
    
    Created copy of project but uses URP (Universal Render Pipeline) vs default renderer. *Note- URP is supported on engine versions that are 2019.4.8 LTS and above (can't go back to 2018.4.26 if continue to use URP).
    
    Fixed building code so that hitting different key will swap the building vs not (result in error as certain structure is expected but wasn't what was placed).
    
    Generated lighting for scene.

---
+++ Wasteland Mech (Universal RP)/Assets/Code/Resource.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public byte health = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.gameObject.name.CompareTo("tree") == 0)
+        {
+            health = 100;
+        }
+        else if (this.gameObject.name.CompareTo("largeRock") == 0)
+        {
+            health = 255;
+        }
+        else if (this.gameObject.name.CompareTo("rustedMetal") == 0)
+        {
+            health = 50;
+        }
+    }
+
+    //when resource is hit
+    public string HitResource(byte damage)
+    {
+        //if health will be above or 0
+        if (this.health - damage >= 0)
+        {
+            //decrease resource health
+            this.health -= damage;
+        }
+        //hit destroys resource
+        else
+        {
+            this.health = 0;
+        }
+
+        //if gameobject has been fully harvested
+        if(this.health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        return this.name;
+    }
+}
---
+++ Wasteland Mech (Universal RP)/Assets/Code/SolarPower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarPower : MonoBehaviour
+{
+    public bool solarGenerate = false;
+    public float power = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //generate power
+        if (solarGenerate)
+        {
+            power += Time.deltaTime;
+        }
+    }
+
+    public void solarEnabled(bool status)
+    {
+        solarGenerate = status;
+        //Debug.Log(this.name + "has updated status");
+    }
+}
