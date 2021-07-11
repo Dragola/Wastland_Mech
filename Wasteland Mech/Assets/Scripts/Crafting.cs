528cad5bbb2369777c887189c9662ba13df514fc
Bryan Gadd <x-dragola-x@live.ca>
2021-07-11 13:31:00 -0400

    Update Packages and Started Crafting Script

---
+++ Wasteland Mech/Assets/Scripts/Crafting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * On hand crafting (limited)
+ * Crafting table/bench V1, V2, etc?
+ * 
+ * On hand crafting = check player inventory
+ * Crafting table/bench = base + player inventory + mech (if docked or have mech storage auto empty [could make option])?
+ */
+public class Crafting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
